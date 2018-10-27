0fbc709a3ab8026094cb062aef19a54f8169a89a
Ilia Onishchenko <wopqw@yandex.ru>
2018-10-27 03:16:40 +0300

    Implement show up of all available cars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarsService} from './cars.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spareshop-frontend';
+
+  constructor(private carsService: CarsService) {}
+
+  availableCars: Car[];
+
+  getAllCars() {
+    this.carsService.getAllCars().subscribe((carSeq: Car[]) => {
+      console.log(carSeq);
+      this.availableCars = carSeq;
+    });
+  }
+
+  isDefined(value: any): boolean {
+    return typeof value !== 'undefined';
+  }
+
+  // noinspection TsLint
+  ngOnInit() {
+    this.getAllCars();
+  }
+}
+
+export interface Car {
+  carId: number;
+  mark: string;
+  model: string;
+  year: number;
+  bodyStyle: string;
 }
---
+++ src/app/cars.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Car } from './app.component';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class CarsService {
+  constructor(private http: HttpClient) {
+  }
+
+  getAllCars(): Observable<Car[]> {
+    return this.http.get<Car[]>('http://localhost:8080/cars')
+  }
+}
